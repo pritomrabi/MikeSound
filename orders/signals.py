713bee14a359a15da062c7e286bf89be795c8b32
Pritom Rabidas <pritomrabidas102@gmail.com>
2025-10-08 18:52:12 +0600

    signal file cart remove

--- orders/signals.py
+++ orders/signals.py
@@ -1,11 +1,8 @@
 from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
+from django.core.mail import send_mail
 from .models import Order, OrderItem
 from inventory.models import Inventory
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.core.mail import send_mail
-from .models import Order
 
 def adjust_inventory_item(item, increase=False):
     try:
@@ -29,7 +26,6 @@ def adjust_sold_count(item, increase=True):
             item.product.sold_count = 0
         item.product.save(update_fields=['sold_count'])
 
-# Order confirmed with paid status
 @receiver(post_save, sender=Order)
 def handle_order_confirmation(sender, instance, **kwargs):
     if instance.status == 'confirmed' and instance.payment_status == 'paid':
@@ -46,7 +42,6 @@ def handle_order_confirmation(sender, instance, **kwargs):
         if cart:
             cart.items.all().delete()
 
-# Order canceled
 @receiver(post_save, sender=Order)
 def handle_order_cancellation(sender, instance, **kwargs):
     if instance.status == 'canceled':
@@ -54,29 +49,41 @@ def handle_order_cancellation(sender, instance, **kwargs):
             adjust_inventory_item(item, increase=True)
             adjust_sold_count(item, increase=False)
 
-# Restore inventory if order deleted
 @receiver(pre_delete, sender=Order)
 def restore_inventory_on_delete(sender, instance, **kwargs):
     for item in instance.items.all():
         adjust_inventory_item(item, increase=True)
         adjust_sold_count(item, increase=False)
 
-
-
-
-
+# -----------------------------
+# Send Mikesound-branded email
+# -----------------------------
 @receiver(post_save, sender=Order)
 def send_order_confirmation_email(sender, instance, created, **kwargs):
     if not created and instance.status == 'confirmed' and instance.payment_status == 'paid':
-        subject = f"Order #{instance.id} Confirmed"
+        subject = f"Mikesound - Order #{instance.id} Confirmed"
         items_text = ""
         for item in instance.items.all():
             items_text += f"{item.product.title} x {item.quantity} ({item.color}) - ${item.line_total}\n"
-        message = f"Hello {instance.address.full_name},\n\nYour order has been confirmed.\n\nProducts:\n{items_text}\nSubtotal: ${instance.subtotal}\nShipping: ${instance.shipping_fee}\nGrand Total: ${instance.grand_total}\n\nThank you for shopping!"
+        message = f"""
+Hello {instance.address.full_name},
+
+Thank you for shopping with Mikesound. Your order has been confirmed.
+
+Order Details:
+{items_text}
+Subtotal: ${instance.subtotal}
+Shipping: ${instance.shipping_fee}
+Grand Total: ${instance.grand_total}
+
+You can visit Mikesound for more products: https://mikesound.com
+
+Thank you for choosing Mikesound!
+"""
         send_mail(
             subject,
             message,
-            None,  # Uses DEFAULT_FROM_EMAIL
+            None,  # DEFAULT_FROM_EMAIL will be used
             [instance.address.email],
             fail_silently=False
         )
